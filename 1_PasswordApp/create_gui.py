4d6954e4a8fdc8e2de694bb6271ef0835f04acce
klabur94 <burkhardt94.christian@web.de>
2024-06-18 13:12:00 +0200

    Implementation of add button

--- 1_PasswordApp/create_gui.py
+++ 1_PasswordApp/create_gui.py
@@ -1,21 +1,30 @@
-# Erstelle eine GUI für deine TO-DO APP
-
 from modules import functions
-import FreeSimpleGUI as sg
+import FreeSimpleGUI as sg  # Library for GUI
 
 # Überschrift
 label = sg.Text("Type in a to do")
 
 # User Input Box
-input_box = sg.InputText(tooltip="Enter Todo", key='todo')  # Füge den Schlüssel 'to do' hinzu
+input_box = sg.InputText(tooltip="Enter Todo", key='todo')  # Füge den Schlüssel 'todo' hinzu
 
 # Button
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+
+# List Box mit den Todos
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='todos',
+                      enable_events=True,
+                      size=(45, 10))
 
 # Anzeigefläche
-window = sg.Window('My to-do App',
-                   layout=[[label], [input_box, add_button]],
-                   font=('Helvetica', 20))
+layout = [
+    [label],
+    [input_box, add_button],
+    [list_box, edit_button]
+]
+window = sg.Window('My to-do App', layout=layout, font=('Helvetica', 20))
+
 # Loop
 while True:
     # Ausgabe Fenster
@@ -28,11 +37,28 @@ while True:
     print(event)
     print(values)
 
-    match event:
-        case "Add":
+    if event == 'Add':
+        todos = functions.get_todos()
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    elif event == 'Edit':
+        if values['todos']:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
             todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        else:
+            sg.popup("Please select a todo to edit")
+
+    elif event == 'todos':
+        if values['todos']:
+            window['todo'].update(value=values['todos'][0])
 
 window.close()
